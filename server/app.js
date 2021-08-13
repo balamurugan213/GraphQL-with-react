c0bc4c8ebb65db836bb730561ba33eddd7aa51bb
balamurugan213 <balainfo21@gmail.com>
2021-08-13 11:10:31 +0530

    security update

--- server/app.js
+++ server/app.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const path = require('path')
+const fs = require('fs');
 const schema=require('./schema/schema') 
 const mongoose=require('mongoose');
 
 const app = express();
 
-//connect ot database
-const uri = "mongodb+srv://bala:qwert12345@dao.fphzg.mongodb.net/LinkedIn?retryWrites=true&w=majority";
-
-
+// Daatabase ORM
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect(uri)
+
+
+//connect to database
+fs.readFile('db-credentails.json', (err, data) => {
+  if (err) throw err;
+  data = JSON.parse(data);
+  mongoose.connect(data.uri)
+})
+
+
+
 
 mongoose.connection.once('open',()=>{
   console.log('connect to database');
